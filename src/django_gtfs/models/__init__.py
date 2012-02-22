9f223180b6bc4213a720db62f8de3d2111ac770c
John Whitlock <John-Whitlock@ieee.org>
2012-02-21 22:37:29 -0600

    Add Agency model

---
+++ src/django_gtfs/models/__init__.py
@@ -0,0 +1,2 @@
+from feed import Feed
+from agency import Agency
--- src/django_gtfs/models/feed.py
+++ src/django_gtfs/models/feed.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Feed(models.Model):
+    """Represents a single GTFS feed.
+    
+    This data is not part of the General Transit Feed Specification.  It is
+    used to allow storage of several GTFS feeds in the same database.
+    """
     name = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add = True)
