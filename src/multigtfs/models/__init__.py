8515c44f18c2999963ddd086be5a171732806952
John Whitlock <John-Whitlock@ieee.org>
2012-04-14 18:23:10 -0500

    Rename calendar -> service

--- src/multigtfs/models/__init__.py
+++ src/multigtfs/models/__init__.py
@@ -1,13 +1,13 @@
 from agency import Agency
 from block import Block
-from calendar import Calendar  # aka service
-from calendar_date import CalendarDate
 from fare_attributes import FareAttributes
 from fare_rules import FareRules
 from feed import Feed
 from feed_info import FeedInfo
 from frequency import Frequency
 from route import Route
+from service import Calendar
+from service_date import CalendarDate
 from shape import Shape, ShapePoint
 from stop import Stop
 from stop_time import StopTime
