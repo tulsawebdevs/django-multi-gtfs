c258ec1e2e70f6ad5111fec6ab714e17ef616595
John Whitlock <jwhitlock@consumeraffairs.com>
2012-09-01 00:39:01 -0500

    Update setup.py for new folder layout

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='multigtfs',
-      version='0.1.2',
+      version='0.1.3,
       description='General Transit Feed Specification (GTFS) as Django app',
       author='John Whitlock',
       author_email='John-Whitlock@ieee.org',
       license='Apache License 2.0',
       url='https://github.com/tulsawebdevs/django-multi-gtfs',
       packages=['multigtfs', 'multigtfs.models'],
-      package_dir={'' : 'src'},
       keywords='django gtfs',
      )
