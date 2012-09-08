03fbfe21c94ad849cd6a88c9044b223072d62b60
John Whitlock <jwhitlock@consumeraffairs.com>
2012-09-08 16:46:47 -0500

    Fix importgtfs management command.  Still needs debug info

--- multigtfs/management/commands/importgtfs.py
+++ multigtfs/management/commands/importgtfs.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from optparse import make_option
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from multigtfs.models.feed import Feed
 
@@ -15,7 +15,11 @@ class Command(BaseCommand):
             help='Set the name of the imported feed'),)
 
     def handle(self, *args, **options):
-        gtfs_feed = args
+        if len(args) == 0:
+            raise CommandError('You must pass in the path to the feed.')
+        if len(args) > 1:
+            raise CommandError('You can only import one feed at a time.')
+        gtfs_feed = args[0]
         name = options.get('name') or 'Imported at %s' % datetime.now()
         feed = Feed.objects.create(name=name)
         feed.import_gtfs(gtfs_feed)
