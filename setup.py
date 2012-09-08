1730d2293f36e5614fda749f091762f94c516464
John Whitlock <jwhitlock@consumeraffairs.com>
2012-09-08 15:15:12 -0500

    Update to 0.2.0

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+# Get the version from __init__.py
+__version__ = None
+execfile('multigtfs/__init__.py')  # Should redefine __version__
 
 setup(
     name='multigtfs',
-    version='0.1.3',
+    version=__version__,
     description='General Transit Feed Specification (GTFS) as Django app',
     author='John Whitlock',
     author_email='John-Whitlock@ieee.org',
     license='Apache License 2.0',
     url='https://github.com/tulsawebdevs/django-multi-gtfs',
-    packages=['multigtfs', 'multigtfs.models'],
+    packages=find_packages(),
+    install_requires=['Django>=1.3'],
     keywords='django gtfs',
 )
