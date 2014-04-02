8ca56f1b209b812f77d93cda2b1c0e1717745352
John Whitlock <John-Whitlock@ieee.org>
2014-04-02 11:33:51 -0500

    Add postgis testing (w/ failing tests)

--- run_tests.py
+++ run_tests.py
@@ -23,8 +23,26 @@ from django.conf import settings
 def main():
     # Dynamically configure the Django settings with the minimum necessary to
     # get Django running tests
-    INSTALLED_APPS = ['multigtfs']
-    TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+    config = {
+        'INSTALLED_APPS': ['multigtfs'],
+        'TEST_RUNNER': 'django.test.simple.DjangoTestSuiteRunner',
+        'DATABASE_ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'DATABASES': {
+            'default': {
+                'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+            }
+        },
+        'DEBUG': True,
+        'TEMPLATE_DEBUG': True
+    }
+
+    try:
+        import south
+    except ImportError:
+        pass
+    else:
+        assert south  # flake8 be quiet
+        config['INSTALLED_APPS'].insert(0, 'south')
 
     # If django-nose is installed, use it
     # You can do things like ./run_tests.py --with-coverage
@@ -37,19 +55,18 @@ def main():
     except DistributionNotFound:
         print "django-nose not installed.  You'd like it."
     else:
-        INSTALLED_APPS.append('django_nose')
-        TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+        config['INSTALLED_APPS'].append('django_nose')
+        config['TEST_RUNNER'] = 'django_nose.NoseTestSuiteRunner'
 
-    settings.configure(
-        INSTALLED_APPS=INSTALLED_APPS,
-        DATABASE_ENGINE='django.contrib.gis.db.backends.spatialite',
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-            }
-        },
-        DEBUG=True, TEMPLATE_DEBUG=True, TEST_RUNNER=TEST_RUNNER
-    )
+    # Optionally update configuration
+    try:
+        import t_overrides
+    except ImportError:
+        pass
+    else:
+        config = t_overrides.update(config)
+
+    settings.configure(**config)
 
     from django.core import management
     failures = management.call_command('test')  # Will pull sysv args itself
