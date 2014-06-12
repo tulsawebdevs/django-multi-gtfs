fe8d5be2bf3c1bfb57cb5227cc262dc0a05aab6e
John Whitlock <John-Whitlock@ieee.org>
2014-06-12 15:47:08 -0500

    Add python3 info to setup

--- setup.py
+++ setup.py
@@ -56,6 +56,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     long_description=(
