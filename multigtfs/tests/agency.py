6a9f5e8e8c1e1be6dadf2ce28f076a56914e99a2
John Whitlock <John-Whitlock@ieee.org>
2014-07-10 20:53:23 -0500

    Import GTFS feeds with BOM
    The byte-order mark can be used to help some programs detect that a file
    is UTF-8 encoded.
    
    Fixes #31

--- multigtfs/tests/agency.py
+++ multigtfs/tests/agency.py
@@ -14,9 +14,10 @@
 # limitations under the License.
 
 from __future__ import unicode_literals
+from codecs import BOM_UTF8
 
 from django.test import TestCase
-from django.utils.six import StringIO
+from django.utils.six import StringIO, PY3
 
 from multigtfs.models import Agency, Feed
 
@@ -83,6 +84,28 @@ DTA,"Demo Transit Authority",http://google.com,America/Los_Angeles,en,\
         self.assertEqual(agency.lang, 'en')
         self.assertEqual(agency.phone, '555-555-TEST')
         self.assertEqual(agency.fare_url, 'http://google.com')
+
+    def test_import_bom(self):
+        if PY3:  # pragma: no cover
+            text = (BOM_UTF8.decode('utf-8') + """\
+agency_name,agency_url,agency_timezone
+Demo Transit Authority,http://google.com,America/Los_Angeles
+""")
+        else:
+            text = (BOM_UTF8 + b"""\
+agency_name,agency_url,agency_timezone
+Demo Transit Authority,http://google.com,America/Los_Angeles
+""")
+        agency_txt = StringIO(text)
+        Agency.import_txt(agency_txt, self.feed)
+        agency = Agency.objects.get()
+        self.assertEqual(agency.agency_id, '')
+        self.assertEqual(agency.name, 'Demo Transit Authority')
+        self.assertEqual(agency.url, 'http://google.com')
+        self.assertEqual(agency.timezone, 'America/Los_Angeles')
+        self.assertEqual(agency.lang, '')
+        self.assertEqual(agency.phone, '')
+        self.assertEqual(agency.fare_url, '')
 
     def test_export_agency_none(self):
         agency_txt = Agency.export_txt(self.feed)
