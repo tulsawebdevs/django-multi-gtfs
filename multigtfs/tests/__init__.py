529e906a11319fe85851b372ed0c410793ffba54
Kevin Diale <powersurge360@gmail.com>
2015-06-10 00:30:15 -0400

    Removed extra imports.
    The imports in question caused django-nose to run the tests twice.

--- multigtfs/tests/__init__.py
+++ multigtfs/tests/__init__.py
@@ -13,34 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import unicode_literals
-
-from multigtfs.tests.test_agency import AgencyTest
-from multigtfs.tests.test_block import BlockTest
-from multigtfs.tests.test_fare import FareTest
-from multigtfs.tests.test_fare_rule import FareRuleTest
-from multigtfs.tests.test_feed import FeedTest
-from multigtfs.tests.test_feed_info import FeedInfoTest
-from multigtfs.tests.test_fields import SecondsTest, SecondsFieldTest
-from multigtfs.tests.test_frequency import FrequencyTest
-from multigtfs.tests.test_route import RouteTest
-from multigtfs.tests.test_service import ServiceTest
-from multigtfs.tests.test_service_date import ServiceDateTest
-from multigtfs.tests.test_shape import ShapeTest
-from multigtfs.tests.test_stop import StopTest
-from multigtfs.tests.test_stop_time import StopTimeTest
-from multigtfs.tests.test_transfer import TransferTest
-from multigtfs.tests.test_trip import TripTest
-from multigtfs.tests.test_zone import ZoneTest
-
-__all__ = (
-    'agency', 'block', 'fare', 'fare_rule', 'feed', 'feed_info', 'fields',
-    'frequency', 'route', 'service', 'service_date', 'shape', 'stop',
-    'stop_time', 'transfer', 'trip', 'utils')
-
-
-# Be quiet pyflakes
-__tests = (
-    AgencyTest, BlockTest, FareTest, FareRuleTest, FeedTest, FeedInfoTest,
-    FrequencyTest, RouteTest, ServiceTest, ServiceDateTest, ShapeTest,
-    ServiceDateTest, ShapeTest, StopTest, StopTimeTest, TransferTest,
-    TripTest, SecondsTest, SecondsFieldTest, ZoneTest)
