3e7ad7a64ae09696d55ef778935461bba23fafe5
John Whitlock <jwhitlock@mozilla.com>
2016-03-01 22:25:32 -0600

    Adjust coverage run

--- qa_check.sh
+++ qa_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 coverage erase
-./run_tests.py --with-coverage
+coverage run --source multigtfs ./run_tests.py
 if [[ $? -ne 0 ]]; then exit 1; fi
+coverage report
 flake8 --exclude=".tox" .
