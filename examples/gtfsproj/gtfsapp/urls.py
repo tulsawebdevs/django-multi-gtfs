084f420168ff80e9f0326183f100ed0e2a40cfee
Adam Lawrence <alaw005@gmail.com>
2017-10-01 01:08:48 +1300

    Most of app running now

--- examples/gtfsproj/gtfsapp/urls.py
+++ examples/gtfsproj/gtfsapp/urls.py
@@ -1,7 +1,22 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
-from gtfsapp.views import FeedList, FeedInfoList, FeedAgencyList, FeedRouteList, FeedStopList, FeedServiceList, FeedShapeList, FeedFareList, FeedZoneList
-from gtfsapp.views import FeedDetail, FeedInfoDetail, AgencyDetail, RouteDetail, StopDetail, ServiceDetail, ShapeDetail, FareDetail, ZoneDetail
+from gtfsapp.views import (
+    FeedList, FeedInfoList, FeedAgencyList, FeedRouteList, 
+    FeedStopList, FeedServiceList, FeedShapeList, FeedFareList, 
+    FeedZoneList, 
+
+    FeedDetail, FeedInfoDetail, AgencyDetail, 
+    RouteDetail, TripDetail, StopDetail, StopTimeDetail, 
+    ServiceDetail, ShapeDetail, ShapePointDetail, FareDetail, 
+    ZoneDetail, 
+
+    StopTimeByStopList, FareRuleByRouteList, TripByRouteList,
+
+    FrequencyByTripList, StopTimeByTripList, 
+
+    TripByShapeList, ShapePointByShapeList,
+
+)
 
 urlpatterns = [
     url(r'^$', FeedList.as_view(), name='feed_list'),
@@ -18,10 +33,26 @@ urlpatterns = [
     url(r'feed/(?P<feed_id>\d+)/feedinfo/(?P<pk>\d+)/$', FeedInfoDetail.as_view(), name='feedinfo_detail'),
     url(r'feed/(?P<feed_id>\d+)/agency/(?P<pk>\d+)/$', AgencyDetail.as_view(), name='agency_detail'),
     url(r'feed/(?P<feed_id>\d+)/route/(?P<pk>\d+)/$', RouteDetail.as_view(), name='route_detail'),
+    url(r'feed/(?P<feed_id>\d+)/trip/(?P<pk>\d+)/$', TripDetail.as_view(), name='trip_detail'),
     url(r'feed/(?P<feed_id>\d+)/stop/(?P<pk>\d+)/$', StopDetail.as_view(), name='stop_detail'),
+    url(r'feed/(?P<feed_id>\d+)/stoptime/(?P<pk>\d+)/$', StopTimeDetail.as_view(), name='stoptime_detail'),
     url(r'feed/(?P<feed_id>\d+)/service/(?P<pk>\d+)/$', ServiceDetail.as_view(), name='service_detail'),
     url(r'feed/(?P<feed_id>\d+)/shape/(?P<pk>\d+)/$', ShapeDetail.as_view(), name='shape_detail'),
+    url(r'feed/(?P<feed_id>\d+)/shapepoint/(?P<pk>\d+)$', ShapePointDetail.as_view(), name='shapepoint_detail'),
+
     url(r'feed/(?P<feed_id>\d+)/fare/(?P<pk>\d+)/$', FareDetail.as_view(), name='fare_detail'),
     url(r'feed/(?P<feed_id>\d+)/zone/(?P<pk>\d+)/$', ZoneDetail.as_view(), name='zone_detail'),
+
+    url(r'feed/(?P<feed_id>\d+)/route/(?P<route_id>\d+)/farerule/$', FareRuleByRouteList.as_view(), name='farerule_by_route_list'),
+    url(r'feed/(?P<feed_id>\d+)/route/(?P<route_id>\d+)/trip/$', TripByRouteList.as_view(), name='trip_by_route_list'),
+
+    url(r'feed/(?P<feed_id>\d+)/trip/(?P<trip_id>\d+)/frequency/$', FrequencyByTripList.as_view(), name='frequency_by_trip_list'),
+    url(r'feed/(?P<feed_id>\d+)/trip/(?P<trip_id>\d+)/stoptime/$', StopTimeByTripList.as_view(), name='stoptime_by_trip_list'),
+
+    url(r'feed/(?P<feed_id>\d+)/stop/(?P<stop_id>\d+)/stoptime/$', StopTimeByStopList.as_view(), name='stoptime_by_stop_list'),
+
+
+    url(r'feed/(?P<feed_id>\d+)/shape/(?P<shape_id>\d+)/shapepoint/$', ShapePointByShapeList.as_view(), name='shapepoint_by_shape_list'),
+    url(r'feed/(?P<feed_id>\d+)/shape/(?P<shape_id>\d+)/trip/$', TripByShapeList.as_view(), name='trip_by_shape_list'),
 
 ]
--- examples/gtfsproj/gtfsapp/views.py
+++ examples/gtfsproj/gtfsapp/views.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from multigtfs.models import Feed, FeedInfo, Agency, Route, Service, Stop, Shape, Fare, Zone
+from multigtfs.models import (
+    Feed, FeedInfo, Agency, Route, Trip, Service, Stop, 
+    Shape, ShapePoint, Fare, Zone, StopTime, FareRule, Frequency
+)
 
 # List feed
 class FeedList(ListView):
@@ -57,6 +60,20 @@ class FeedFareList(ByFeedListView):
     model = Fare
 
 
+class StopTimeByStopList(ListView):
+    model = StopTime
+
+    def get_context_data(self, **kwargs):
+        context = super(StopTimeByStopList, self).get_context_data(
+            **kwargs)
+        context['stop'] = Stop.objects.get(id=self.kwargs['stop_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return StopTime.objects.filter(stop_id=self.kwargs['stop_id'])
+
+
 # Detail
 class FeedDetail(DetailView):
     model = Feed
@@ -70,11 +87,20 @@ class AgencyDetail(DetailView):
 class StopDetail(DetailView):
     model = Stop
 
+class StopTimeDetail(DetailView):
+    model = StopTime
+
 class RouteDetail(DetailView):
     model = Route
 
+class TripDetail(DetailView):
+    model = Trip
+
 class ShapeDetail(DetailView):
     model = Shape
+
+class ShapePointDetail(DetailView):
+    model = ShapePoint
 
 class ServiceDetail(DetailView):
     model = Service
@@ -84,4 +110,88 @@ class ZoneDetail(DetailView):
 
 class FareDetail(DetailView):
     model = Fare
+
+
+class TripByRouteList(ListView):
+    model = Trip
+
+    def get_context_data(self, **kwargs):
+        context = super(TripByRouteList, self).get_context_data(**kwargs)
+        context['route'] = Route.objects.get(id=self.kwargs['route_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return Trip.objects.filter(route_id=self.kwargs['route_id'])
+
+
+class FareRuleByRouteList(ListView):
+    model = FareRule
+
+    def get_context_data(self, **kwargs):
+        context = super(FareRuleByRouteList, self).get_context_data(
+            **kwargs)
+        context['route'] = Route.objects.get(id=self.kwargs['route_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return FareRule.objects.filter(route_id=self.kwargs['route_id'])
+
+
+class StopTimeByTripList(ListView):
+    model = StopTime
+
+    def get_context_data(self, **kwargs):
+        context = super(StopTimeByTripList, self).get_context_data(
+            **kwargs)
+        context['trip'] = Trip.objects.get(id=self.kwargs['trip_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return StopTime.objects.filter(trip=self.kwargs['trip_id'])
+
+
+class FrequencyByTripList(ListView):
+    model = Frequency
+
+    def get_context_data(self, **kwargs):
+        context = super(FrequencyByTripList, self).get_context_data(
+            **kwargs)
+        context['trip'] = Trip.objects.get(id=self.kwargs['trip_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return Frequency.objects.filter(trip=self.kwargs['trip_id'])
+
+
+
+class TripByShapeList(ListView):
+    model = ShapePoint
+
+    def get_context_data(self, **kwargs):
+        context = super(TripByShapeList, self).get_context_data(**kwargs)
+        context['shape'] = Shape.objects.get(id=self.kwargs['shape_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return Trip.objects.filter(shape=self.kwargs['shape_id'])
+
+class ShapePointByShapeList(ListView):
+    model = ShapePoint
+
+    def get_context_data(self, **kwargs):
+        context = super(ShapePointByShapeList, self).get_context_data(
+            **kwargs)
+        context['shape'] = Shape.objects.get(id=self.kwargs['shape_id'])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        return ShapePoint.objects.filter(shape=self.kwargs['shape_id'])
+
+
 
