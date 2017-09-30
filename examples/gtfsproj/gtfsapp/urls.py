d1ea34152b1a1ed21c59f644260d0b907a4a61a8
Adam Lawrence <alaw005@gmail.com>
2017-10-01 00:11:41 +1300

    List views created from other eg

--- examples/gtfsproj/gtfsapp/urls.py
+++ examples/gtfsproj/gtfsapp/urls.py
@@ -1,8 +1,27 @@
 from django.conf.urls import url
 
-from gtfsapp.views import FeedList, FeedDetail
+from gtfsapp.views import FeedList, FeedInfoList, FeedAgencyList, FeedRouteList, FeedStopList, FeedServiceList, FeedShapeList, FeedFareList, FeedZoneList
+from gtfsapp.views import FeedDetail, FeedInfoDetail, AgencyDetail, RouteDetail, StopDetail, ServiceDetail, ShapeDetail, FareDetail, ZoneDetail
 
 urlpatterns = [
     url(r'^$', FeedList.as_view(), name='feed_list'),
+    url(r'feed/(?P<feed_id>\d+)/feedinfo/$', FeedInfoList.as_view(), name='feedinfo_list'),
+    url(r'feed/(?P<feed_id>\d+)/agency/$', FeedAgencyList.as_view(), name='agency_list'),
+    url(r'feed/(?P<feed_id>\d+)/route/$', FeedRouteList.as_view(), name='route_list'),
+    url(r'feed/(?P<feed_id>\d+)/stop/$', FeedStopList.as_view(), name='stop_list'),
+    url(r'feed/(?P<feed_id>\d+)/service/$', FeedServiceList.as_view(), name='service_list'),
+    url(r'feed/(?P<feed_id>\d+)/shape/$', FeedShapeList.as_view(), name='shape_list'),
+    url(r'feed/(?P<feed_id>\d+)/fare/$', FeedFareList.as_view(), name='fare_list'),
+    url(r'feed/(?P<feed_id>\d+)/zone/$', FeedZoneList.as_view(), name='zone_list'),
+
     url(r'^feed/(?P<pk>\d+)/$', FeedDetail.as_view(), name='feed_detail'),
+    url(r'feed/(?P<feed_id>\d+)/feedinfo/(?P<pk>\d+)/$', FeedInfoDetail.as_view(), name='feedinfo_detail'),
+    url(r'feed/(?P<feed_id>\d+)/agency/(?P<pk>\d+)/$', AgencyDetail.as_view(), name='agency_detail'),
+    url(r'feed/(?P<feed_id>\d+)/route/(?P<pk>\d+)/$', RouteDetail.as_view(), name='route_detail'),
+    url(r'feed/(?P<feed_id>\d+)/stop/(?P<pk>\d+)/$', StopDetail.as_view(), name='stop_detail'),
+    url(r'feed/(?P<feed_id>\d+)/service/(?P<pk>\d+)/$', ServiceDetail.as_view(), name='service_detail'),
+    url(r'feed/(?P<feed_id>\d+)/shape/(?P<pk>\d+)/$', ShapeDetail.as_view(), name='shape_detail'),
+    url(r'feed/(?P<feed_id>\d+)/fare/(?P<pk>\d+)/$', FareDetail.as_view(), name='fare_detail'),
+    url(r'feed/(?P<feed_id>\d+)/zone/(?P<pk>\d+)/$', ZoneDetail.as_view(), name='zone_detail'),
+
 ]
--- examples/gtfsproj/gtfsapp/views.py
+++ examples/gtfsproj/gtfsapp/views.py
@@ -2,13 +2,86 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
 from django.views.generic import ListView, DetailView
-from multigtfs.models import Feed
 
+from multigtfs.models import Feed, FeedInfo, Agency, Route, Service, Stop, Shape, Fare, Zone
+
+# List feed
 class FeedList(ListView):
-    #model = Feed
     queryset = Feed.objects.order_by('name', '-created')
 
+# Filter lists by feed
+class ByFeedListView(ListView):
+    by_col = 'feed_id'
+    by_kwarg = 'feed_id'
+    by_class = Feed
+    by_classname = 'feed'
+
+    def get_context_data(self, **kwargs):
+        context = super(ByFeedListView, self).get_context_data(
+            **kwargs)
+        context[self.by_classname] = self.by_class.objects.get(
+            id=self.kwargs[self.by_kwarg])
+        context['feed_id'] = self.kwargs['feed_id']
+        return context
+
+    def get_queryset(self, **kwargs):
+        q_filter = {self.by_col: self.kwargs[self.by_kwarg]}
+        qset = super(ByFeedListView, self).get_queryset(**kwargs)
+        return qset.filter(**q_filter)
+
+
+# List
+class FeedInfoList(ByFeedListView):
+    model = FeedInfo
+
+class FeedAgencyList(ByFeedListView):
+    model = Agency
+
+class FeedStopList(ByFeedListView):
+    model = Stop
+
+class FeedRouteList(ByFeedListView):
+    model = Route
+
+class  FeedShapeList(ByFeedListView):
+    model = Shape
+
+class FeedServiceList(ByFeedListView):
+    model = Service
+
+class FeedZoneList(ByFeedListView):
+    model = Zone
+
+class FeedFareList(ByFeedListView):
+    model = Fare
+
+
+# Detail
 class FeedDetail(DetailView):
     model = Feed
+
+class FeedInfoDetail(DetailView):
+    model = FeedInfo
+
+class AgencyDetail(DetailView):
+    model = Agency
+
+class StopDetail(DetailView):
+    model = Stop
+
+class RouteDetail(DetailView):
+    model = Route
+
+class ShapeDetail(DetailView):
+    model = Shape
+
+class ServiceDetail(DetailView):
+    model = Service
+
+class ZoneDetail(DetailView):
+    model = Zone
+
+class FareDetail(DetailView):
+    model = Fare
+
