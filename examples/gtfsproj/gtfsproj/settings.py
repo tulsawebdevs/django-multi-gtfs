231f5a17698e77544a96f5433067f16edbba8802
Adam Lawrence <alaw005@gmail.com>
2017-09-30 18:48:13 +1300

    Now works with postgis too with eg

--- examples/gtfsproj/gtfsproj/settings.py
+++ examples/gtfsproj/gtfsproj/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
 
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -107,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config('TIME_ZONE', default='UTC')
 
 USE_I18N = True
 
