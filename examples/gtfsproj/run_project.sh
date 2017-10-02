26d2b70cc32819068badc957f0615f2acee30f76
Adam Lawrence <alaw005@gmail.com>
2017-10-03 11:25:38 +1300

    Docker almost working

--- examples/gtfsproj/run_project.sh
+++ examples/gtfsproj/run_project.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -e
 
 # Create environment variables if not exist
-ENVIRONMENT_FILE=.env2
+ENVIRONMENT_FILE=".env"
 if [ ! -f $ENVIRONMENT_FILE ]; then
-    echo "Create default environment from example"
-    cp .env.example $ENVIRONMENT_FILE
+  echo "Create default parameters using $ENVIRONMENT_FILE file"
+  cp .env.example $ENVIRONMENT_FILE
 fi
 
-./manage.py runserver 0.0.0.0:8000
+# If no arguments provided then run server on port 8000
+if [ $# -eq 0 ]; then
+  # If no arguments provided then run server on port 8000
+  exec ./manage.py runserver 0.0.0.0:8000
+else
+  if [ $1 = "bash" ]; then
+    # If bash first argument then use that for all arguments
+    exec "$@"
+  else
+    # Otherwise pass arguments to django manage.py script
+    exec ./manage.py "$@"
+  fi
+fi
+
